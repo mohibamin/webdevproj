0ca2b9ffead759c0a6e39a319783e6ba3db45191
mohibamin <129216337+mohibamin@users.noreply.github.com>
2023-03-29 22:42:55 -0400

    Add files via upload

---
+++ Website/README.md
@@ -0,0 +1,1 @@
+![Screen Shot 2020-09-12 at 10 05 50 PM](https://user-images.githubusercontent.com/71200038/93008611-9b482800-f544-11ea-82f4-57d63ebfdbad.png)
---
+++ Website/Untitled-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int
+main (void)
+{
+	int a; double b = 65.5;
+	
+	printf("Enter a number: ");
+	scanf("%d",&a);
+	printf("The number is %d. \n", a);
+	return(0);
+}
